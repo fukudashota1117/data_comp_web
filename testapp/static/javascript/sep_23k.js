446b690eb38be6de1305a4287d2eaf40cba2fd7b
fukudashota1117 <azutaro1117@toki.waseda.jp>
2022-11-20 23:36:25 +0900

    ranking

--- testapp/static/javascript/sep_23k.js
+++ testapp/static/javascript/sep_23k.js
@@ -12,7 +12,10 @@ third_k=third_k.innerHTML
 function create_html(ku) {
     if (ku == arr_23k[4]) {
         var h4 = 
-        '<p>文京区</p>'
+        '文京区</p>'
+        +'<img src'
+        +'="/static/images/23k_picture/文京区.jpg"'
+        +'width="680px" height="450px">'
         +'<p>'
         +'大学が多い文教地区と閑静な住宅街が多くを占めている区です。住宅街、公園、教育機関、医療機関が多く点在しており治安が良く、暮らしたい街ランキングでは常に上位に位置する区です。'
         +'</p>';
@@ -20,26 +23,32 @@ function create_html(ku) {
     }
     else if (ku == arr_23k[11]) {
         var h11 = 
-        '<p>世田谷区</p>'
+        '世田谷区</p>'
+        +'<img src'
+        +'="/static/images/23k_picture/世田谷区.jpg"'
+        +'width="680px" height="450px">'
         +'<p>'
         +'子育て支援やサポートも充実しており、育児しやすい街としても人気が高い区です。 自然も多く、公園も充実しており、ペットや小さな子どもがいるファミリーにもおすすめ交通の便がよい、緑の豊かな区でもあります。'
         +'</p>';
         return h11;
     }
     else if (ku == arr_23k[17]) {
-        var h16 = 
-        '<p>荒川区</p>'
+        var h17 = 
+        '荒川区</p>'
+        +'<img src'
+        +'="/static/images/23k_picture/荒川区.jpg"'
+        +'width="680px" height="450px">'
         +'<p>'
         +'再開発も進んでおらず、下町の落ち着いた雰囲気が残る街が多い、大きな繁華街も無く治安が良い区です。治安ナンバーワン都市を目指して防犯対策を強化しており、年々犯罪数が減っています。さまざまな子育て支援策もあり「子育てしやすい街」という評価も。'
         +'</p>';
-        return h16;
+        return h17;
     }
 }//それぞれのhtml作成用関数
 
-ht1 = create_html(first_k);
+ht1 = '<p>'+'<img src="/static/images/1st.png" '+'width="50" height="50">'+create_html(first_k);
 document.write(ht1);
-ht2 = create_html(second_k);
+ht2 = '<p>'+'<img src="/static/images/2nd.png" '+'width="50"" height="50">'+create_html(second_k);
 document.write(ht2);
-ht3 = create_html(third_k);
+ht3 = '<p>'+'<img src="/static/images/3rd.png" '+'width="50" height="50">'+create_html(third_k);
 document.write(ht3);
 
